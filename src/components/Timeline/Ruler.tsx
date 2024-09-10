26a507347d65097e2b5a8c50803507ec71229c1d
Alvaro <alvarolukmanto@gmail.com>
2024-09-11 01:31:07 +0800

    feat: implement basic ruler dragging to set time

--- src/components/Timeline/Ruler.tsx
+++ src/components/Timeline/Ruler.tsx
@@ -1,5 +1,85 @@
-export const Ruler = () => {
+import { useCallback, useRef } from "react";
+
+type RulerProps = {
+  width: number;
+  onMouseDown: (time: number) => void;
+  onDragging: (time: number) => void;
+  onDraggingEnd: () => void;
+};
+
+export const Ruler = ({
+  width,
+  onMouseDown,
+  onDragging,
+  onDraggingEnd
+}: RulerProps) => {
   // TODO: implement mousedown and mousemove to update time and Playhead position
+  const rulerRef = useRef(null);
+  const isDraggable = useRef<boolean>(false);
+  const boundary = useRef({
+    left: 0,
+    right: 0
+  });
+
+  // useEffect(() => {
+  //   window.addEventListener("mouseup", endDragging);
+  //   window.addEventListener("mousemove", handleDragging);
+
+  //   return () => {
+  //     window.removeEventListener("mouseup", endDragging);
+  //     window.removeEventListener("mousemove", handleDragging);
+  //   };
+  // }, []);
+
+  const handleMouseDown = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      const element = e.target as Element;
+      const elementRect = element.getBoundingClientRect();
+
+      isDraggable.current = true;
+      boundary.current = {
+        left: elementRect.left,
+        right: elementRect.right
+      };
+
+      onMouseDown(e.clientX - boundary.current.left);
+      enableDragging();
+    },
+    [onMouseDown]
+  );
+
+  function handleDragging(e: MouseEvent) {
+    if (!isDraggable.current) {
+      return;
+    }
+
+    if (e.clientX < boundary.current.left) {
+      onDragging(0);
+      return;
+    }
+
+    if (e.clientX > boundary.current.right) {
+      onDragging(width);
+      return;
+    }
+
+    requestAnimationFrame(() => {
+      onDragging(e.clientX - boundary.current.left);
+    });
+  }
+
+  function endDragging() {
+    isDraggable.current = false;
+    onDraggingEnd();
+
+    window.removeEventListener("mousemove", handleDragging);
+    window.removeEventListener("mouseup", endDragging);
+  }
+
+  function enableDragging() {
+    window.addEventListener("mousemove", handleDragging);
+    window.addEventListener("mouseup", endDragging);
+  }
 
   return (
     <div
@@ -8,7 +88,12 @@ export const Ruler = () => {
       overflow-x-auto overflow-y-hidden"
       data-testid="ruler"
     >
-      <div className="w-[2000px] h-6 rounded-md bg-white/25"></div>
+      <div
+        ref={rulerRef}
+        className="w-[2000px] h-6 rounded-md bg-white/25"
+        onMouseDown={handleMouseDown}
+        style={{ width }}
+      ></div>
     </div>
   );
 };
--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Playhead } from "./Playhead";
 import { Ruler } from "./Ruler";
 import { TrackList } from "./TrackList";
 import { KeyframeList } from "./KeyframeList";
 import { PlayControls } from "./PlayControls";
+import { roundToNearestPrecision } from "../../utils/math";
 
 export const Timeline = () => {
   // FIXME: performance concerned
   const [time, setTime] = useState(0);
   const [duration, setDuration] = useState(2000);
+  const [playheadX, setPlayheadX] = useState(time);
+  const isDraggingOnRuler = useRef<boolean>(false);
+
+  useEffect(() => {
+    // on ruler mouse move, Playhead movement is per 1px while PlayControl input increment is per 10px.
+    // this flag is to prevent playhead to move by 10px while dragging
+    if (!isDraggingOnRuler.current) {
+      setPlayheadX(time);
+    }
+  }, [time]);
+
+  function handleRulerDragging(time: number) {
+    isDraggingOnRuler.current = true;
+
+    setTime(roundToNearestPrecision(time, 10));
+    setPlayheadX(time);
+  }
+
+  function handleRulerMouseDown(time: number) {
+    setTime(roundToNearestPrecision(time, 10));
+  }
+
+  function handleRulerDraggingEnd() {
+    isDraggingOnRuler.current = false;
+  }
 
   return (
     <div
@@ -22,10 +48,15 @@ export const Timeline = () => {
         duration={duration}
         setDuration={setDuration}
       />
-      <Ruler />
+      <Ruler
+        width={duration}
+        onMouseDown={handleRulerMouseDown}
+        onDragging={handleRulerDragging}
+        onDraggingEnd={handleRulerDraggingEnd}
+      />
       <TrackList />
       <KeyframeList />
-      <Playhead time={time} />
+      <Playhead time={playheadX} />
     </div>
   );
 };
