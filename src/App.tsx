b79bc2f787f2712b2a141837bb9c1ae5c3282255
Alvaro <alvarolukmanto@gmail.com>
2024-09-13 00:23:32 +0800

    style: move timeline to the middle of screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { Timeline } from "./components/Timeline";
 export default function App() {
   return (
     <div className="flex flex-col h-dvh bg-gray-900 text-white">
-      <div className="p-10 flex-grow">
+      <div className="p-10">
         <h1 className="text-2xl mb-4 text-gray-50">Phase Timeline Challenge</h1>
         <p className="text-gray-300">
           Please follow the instructions in the README.md.
         </p>
       </div>
-      <Timeline />
+      <div className="p-10">
+        <Timeline />
+      </div>
     </div>
   );
 }
